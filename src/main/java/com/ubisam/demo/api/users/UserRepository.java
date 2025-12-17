5d9ffcf6b165e38b80e4563d0cbe844e49e13846
ParkJinwon1025 <jinwon2812@ubisam.com>
2025-12-17 16:38:43 +0900

    코드 수정

--- src/main/java/com/ubisam/demo/api/users/UserRepository.java
+++ src/main/java/com/ubisam/demo/api/users/UserRepository.java
@@ -4,6 +4,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ubisam.demo.domain.User;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
