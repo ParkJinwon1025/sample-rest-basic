c0a81bd860873ac0e46532ce0c7d7dc7967953ca
ParkJinwon1025 <jinwon2812@ubisam.com>
2026-01-12 15:08:56 +0900

    임시 구조 수정

--- src/main/java/com/ubisam/demo/domain/User.java
+++ src/main/java/com/ubisam/demo/domain/User.java
@@ -1,7 +1,5 @@
 package com.ubisam.demo.domain;
 
-import java.util.UUID;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
